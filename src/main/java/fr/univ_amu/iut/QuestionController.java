10d0e1e9c6006f9e8b966f87e5ff45691206d247
LennyGonzales <lenny.gonzales@etu.univ-amu.fr>
2022-12-12 20:00:11 +0100

    Added a description of the situation for each question. The user can now write his answer himself (Now there are two types of answers to the questions. The user can have a multiple-choice question or a question where he has to write the answer themselves.)

--- src/main/java/fr/univ_amu/iut/QuestionController.java
+++ src/main/java/fr/univ_amu/iut/QuestionController.java
@@ -1,9 +1,11 @@
 package fr.univ_amu.iut;
 
 import fr.univ_amu.iut.client.Client;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -12,18 +14,27 @@ import java.util.HashMap;
  */
 public class QuestionController {
     @FXML
+    private VBox vboxParent;
+    @FXML
     private Label question;
+    @FXML
+    private Label description;
     @FXML
     private CheckBox answer1;
     @FXML
     private CheckBox answer2;
     @FXML
     private CheckBox answer3;
+    @FXML
+    private TextField writtenResponseTextField;
     private final Client client;
     private HashMap<String, Boolean> answersStatus;
+    private Font font;
+
     public QuestionController() {
         client = Main.getClient();
         answersStatus = new HashMap<>();
+        font = new Font("System Bold", 25);
     }
 
     /**
@@ -39,25 +50,83 @@ public class QuestionController {
      * @throws IOException
      * @throws InterruptedException
      */
-    public void initializeVariables(String questionText) throws IOException {
-        question.setText(questionText);        // We are obliged to do this for the endgame check
+    public void initializeVariables(String answerType) throws IOException {
+        question.setText(client.receiveMessageFromServer());        // We are obliged to do this for the endgame check
+        description.setText(client.receiveMessageFromServer());
+        if(answerType.equals("QCM_FLAG")) {
+            createCheckBoxes();
+            initializeTextCheckBoxes();
+        } else if (answerType.equals("WRITTEN_RESPONSE_QUESTION_FLAG")) {
+            createWrittenResponse();
+        }
+    }
+
+    /**
+     * Create the checkboxes by adding them in the fxml, fix their fx:id and their font
+     */
+    public void createCheckBoxes() {
+        for (int numCheckBox = 1; numCheckBox < 4; ++numCheckBox) {
+            CheckBox checkBoxAnswer = new CheckBox();
+            checkBoxAnswer.setId("answer" + numCheckBox);
+            checkBoxAnswer.setFont(font);
+            checkBoxAnswer.setStyle("-fx-text-fill: #e7e7e7");
+            vboxParent.getChildren().add(numCheckBox+2,checkBoxAnswer); // Puts this checkbox at the good position (after the title, description and question | before the button to submit and leave)
+        }
+        answer1 = ((CheckBox) vboxParent.lookup("#answer1"));
+        answer2 = ((CheckBox) vboxParent.lookup("#answer2"));
+        answer3 = ((CheckBox) vboxParent.lookup("#answer3"));
+        System.out.println("QCM after creation : " + vboxParent.getChildren().size());
+
+    }
+
+    /**
+     * Set the text of the checkboxes
+     * @throws IOException
+     */
+    public void initializeTextCheckBoxes() throws IOException {
         answer1.setText(client.receiveMessageFromServer());
         answer2.setText(client.receiveMessageFromServer());
         answer3.setText(client.receiveMessageFromServer());
     }
 
     /**
-     * Check if there are no more question
+     * Create the TextField by adding it in the fxml and set his font
+     */
+    public void createWrittenResponse() {
+        TextField textField = new TextField();
+        textField.setFont(font);
+        textField.setPrefWidth(1000);
+        textField.setId("answer");
+        vboxParent.getChildren().add(3, textField);
+        writtenResponseTextField = textField;   //-----------------------
+        System.out.println("WrittenResponse after creation : " + vboxParent.getChildren().size());
+    }
+
+    /**
+     * Submit the question to the server
      * @throws IOException
      * @throws InterruptedException
      */
-    public void verifyEndGame() throws IOException {
-        String message = client.receiveMessageFromServer();
-        if(message.equals("END_GAME_FLAG")) {
-            endGame();
-        } else {
-            initializeVariables(message);
+    public void submitAnswer() throws IOException {
+        if(vboxParent.getChildren().size() <= 6) {  // If the response is a written response
+            client.sendMessageToServer(writtenResponseTextField.getText());
+            vboxParent.getChildren().remove(writtenResponseTextField); // Remove the TextField
+
+        } else {    // If it's a QCM
+            if (answer1.isSelected()) {
+                client.sendMessageToServer("1");
+            } else if (answer2.isSelected()) {
+                client.sendMessageToServer("2");
+            } else if (answer3.isSelected()) {
+                client.sendMessageToServer("3");
+            } else {
+                client.sendMessageToServer("0");
+            }
+            vboxParent.getChildren().remove(3,6);   // Remove all the checkboxes
         }
+        System.out.println("after deleting : " + vboxParent.getChildren().size());
+
+        answerStatus(); // Check if the answer is correct or wrong and add it to the hash map
     }
 
     /**
@@ -71,37 +140,23 @@ public class QuestionController {
         } else {
             answersStatus.put(question.getText(), false);
         }
-
         verifyEndGame();
     }
 
-    /**
-     * Uncheck all the checkbox
-     */
-    public void unselectCheckBox() {
-        answer1.setSelected(false);
-        answer2.setSelected(false);
-        answer3.setSelected(false);
-    }
 
     /**
-     * Submit the question to the server
+     * Check if there are no more question
      * @throws IOException
      * @throws InterruptedException
      */
-    public void submitQuestion() throws IOException {
-        if(answer1.isSelected()) {
-            client.sendMessageToServer("1");
-        } else if(answer2.isSelected()) {
-            client.sendMessageToServer("2");
-        } else if (answer3.isSelected()) {
-            client.sendMessageToServer("3");
+    public void verifyEndGame() throws IOException {
+        String message = client.receiveMessageFromServer();
+        if(message.equals("END_GAME_FLAG")) {
+            endGame();
         } else {
-            client.sendMessageToServer("0");
+            System.out.println("Size : " + vboxParent.getChildren().size());
+            initializeVariables(message);
         }
-
-        unselectCheckBox();
-        answerStatus();
     }
 
     /**
