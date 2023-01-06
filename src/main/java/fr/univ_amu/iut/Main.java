090c9319bc6129bfec847851a99e86d95c638e6a
LennyGonzales <lenny.gonzales@etu.univ-amu.fr>
2023-01-06 09:02:02 +0100

    Change the dimension of the window

--- src/main/java/fr/univ_amu/iut/Main.java
+++ src/main/java/fr/univ_amu/iut/Main.java
@@ -32,13 +32,9 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
 
         // Screen settings
-        Screen screen = Screen.getPrimary();
-        Rectangle2D bounds = screen.getVisualBounds();
-        stage.setWidth(bounds.getWidth());
-        stage.setHeight(bounds.getHeight());
         stage.setMaximized(true);
         stage.minHeightProperty().set(HEIGHT);
         stage.minWidthProperty().set(WIDTH);
